3e06290d96017bb44f6d9a22332d6ac6cbd2e86a
Denzel <dbrown211@student.gsu.edu>
2018-12-04 14:16:05 -0500

    Modified data directory location

--- continuous.sh
+++ continuous.sh
@@ -1,2 +1,7 @@
-mkdir -p data
-while :; do ./drsLog $(cat config.txt); done;
+#!/bin/bash
+mkdir -p /var/www/html/data
+while :;
+do
+    ./drsLog $(cat config.txt);
+    cat /var/www/html/data/* > /var/www/html/data/all.dat
+done;
