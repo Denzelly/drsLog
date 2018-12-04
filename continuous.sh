3c307303692ab539207c2be43f421681b53631b0
Denzel <dbrown211@student.gsu.edu>
2018-12-04 14:27:04 -0500

    Changed all.dat directory. Made sure script is run as root

--- continuous.sh
+++ continuous.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+#Check if running as root
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Please run as root"
+  exit
+fi
+
 mkdir -p /var/www/html/data
 while :;
 do
     ./drsLog $(cat config.txt);
-    cat /var/www/html/data/* > /var/www/html/data/all.dat
+    cat /var/www/html/data/* > /var/www/html/all.dat
 done;
